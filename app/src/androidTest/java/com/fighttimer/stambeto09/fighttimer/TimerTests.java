e2b15a3cbb38f7b0c354d7cd4a9c633e285faab7
stambeto09 <akastambata@gmail.com>
2015-02-12 09:31:30 +0200

    Adds TimerTests file

---
+++ app/src/androidTest/java/com/fighttimer/stambeto09/fighttimer/TimerTests.java
@@ -0,0 +1,12 @@
+package com.fighttimer.stambeto09.fighttimer;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by stambeto09 on 15-2-12.
+ */
+public class TimerTests extends TestCase{
+    public void testInitializingTimer(){
+        Timer testTimer = new Timer(0, 0);
+    }
+}
